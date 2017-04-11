709fbe682c02ac9da489bd0f90b661aae5003b26
Grayson Hicks <graysonhicks@gmail.com>
2017-04-11 08:42:49 -0400

    basic index

---
+++ app/index.js
@@ -0,0 +1,22 @@
+import React, {
+    Component
+} from 'react';
+import ReactDOM from 'react-dom';
+require('./index.css');
+
+
+// state
+// lifecycle
+// ui
+class App extends Component {
+    render() {
+        return (
+            <h1> Battlefield </h1>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App /> ,
+    document.getElementById('app');
+)
